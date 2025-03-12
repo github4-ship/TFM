576526ecc6fe20fff04bd506d944b12d1c54e98a
Aitor <155718438+github4-ship@users.noreply.github.com>
2025-03-12 20:00:19 +0100

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ authenticator = stauth.Authenticate(
     cookie_expiry_days=30
 )
 
-name, authentication_status, username = authenticator.login('Login', 'sidebar')
+name, authentication_status, username = authenticator.login('Login', location='sidebar')
 
 if authentication_status == False:
     st.error('❌ Usuario o contraseña incorrectos.')
